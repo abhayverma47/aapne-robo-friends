31cd1457e4ae185b43544bd2227d7beb2645e2ad
abhayverma47 <vabhay2012@gmail.com>
2019-12-13 10:24:05 +0530

    border removed

--- robo/src/components/scroll.js
+++ robo/src/components/scroll.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '1px solid black', height: "800px" }}>
+        <div style={{ overflowY: 'scroll', height: "800px" }}>
             {props.children}
         </div>
     );
 }
 export default Scroll;
+
+//{border: '1px solid black',}
