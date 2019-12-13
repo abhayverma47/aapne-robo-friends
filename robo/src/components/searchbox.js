f9ba2543dfe6585a10cac8628f72d64ff562255c
abhayverma47 <vabhay2012@gmail.com>
2019-12-13 10:26:13 +0530

    lightest to light

--- robo/src/components/searchbox.js
+++ robo/src/components/searchbox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Searchbox = ({ searchchange }) => {
     return (
         <div className='pa2'>
-            <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='Search' onChange={searchchange} />
+            <input className='pa3 ba b--green bg-light-blue' type='search' placeholder='Search' onChange={searchchange} />
         </div>
     );
 }
